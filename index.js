6ca2cc88ea43b19f19786263a2f5d6b5cb6c5f86
Lolllipoppp <102265928+Lolllipoppp@users.noreply.github.com>
2022-07-17 21:40:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /**
-  * Created by Riy
-  * Base Ori : rtwone / Irfan
+  * Edit features in './message/msg.js'
+  * Contact me on WhatsApp wa.me/6281319944917
+  * Follow : https://github.com/rtwone
+  * Follow : https://github.com/GetSya
 */
 
-"use strict";
+//"use strict"; default mode
 const {
 	default: makeWASocket,
 	BufferJSON,
@@ -29,17 +31,7 @@ let session = `./${setting.sessionName}.json`
 const { state, saveState } = useSingleFileAuthState(session)
 const { groupResponse } = require('./message/group.js')
 
-function title() {
-      console.clear()
-	  console.log(chalk.bold.green(figlet.textSync('Hinata Hyuga', {
-		font: 'Standard',
-		horizontalLayout: 'default',
-		verticalLayout: 'default',
-		width: 80,
-		whitespaceBreak: false
-	})))
-	console.log(chalk.yellow(`\n                        ${chalk.yellow('[ Created By Riy ]')}\n\n${chalk.red('Hinata Hyuga Bot')} : ${chalk.white('WhatsApp Bot Multi Device')}\n${chalk.red('Follow Insta Dev')} : ${chalk.white('@riychdwayne')}\n${chalk.red('Message Me On WhatsApp')} : ${chalk.white('6281575886399')}\n${chalk.red('Donate')} : ${chalk.white('081575886399 ( Dana/Pulsa )')}\n`))
-}
+function _0x2943(_0x12e420,_0x504a3f){var _0x39851f=_0x3985();return _0x2943=function(_0x2943ce,_0x135cb8){_0x2943ce=_0x2943ce-0x1e9;var _0xc6790=_0x39851f[_0x2943ce];return _0xc6790;},_0x2943(_0x12e420,_0x504a3f);}(function(_0x189a90,_0x1ec7f4){var _0x29f92c=_0x2943,_0x537ebb=_0x189a90();while(!![]){try{var _0x31ae9a=parseInt(_0x29f92c(0x1f8))/0x1*(parseInt(_0x29f92c(0x1fc))/0x2)+parseInt(_0x29f92c(0x1f2))/0x3+-parseInt(_0x29f92c(0x1fe))/0x4+-parseInt(_0x29f92c(0x1ea))/0x5+parseInt(_0x29f92c(0x1ee))/0x6*(-parseInt(_0x29f92c(0x1ed))/0x7)+-parseInt(_0x29f92c(0x1eb))/0x8+parseInt(_0x29f92c(0x1ef))/0x9;if(_0x31ae9a===_0x1ec7f4)break;else _0x537ebb['push'](_0x537ebb['shift']());}catch(_0x358ee0){_0x537ebb['push'](_0x537ebb['shift']());}}}(_0x3985,0x5b215));function _0x3985(){var _0x995888=['1660863fudiVN','textSync','Message\x20Me\x20On\x20WhatsApp','[BASE\x20BY\x20ARASYA]','088213292687\x20(\x20Gopay/Pulsa\x20)','green','229055PhmgfT','log','white','+62\x20813-1994-4917','2FTvpEM','WhatsApp\x20Bot\x20Multi\x20Device','1195760qoqfAN','bold','DEFF-MD','Standard','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20','\x20:\x20','yellow','red','2779140mEKwnL','783288OWhvSa','Donate','161UnLhwK','110118CiUqlC','8688519hClJhI','default','Follow\x20Insta\x20Dev'];_0x3985=function(){return _0x995888;};return _0x3985();}function title(){var _0x1cf7b1=_0x2943;console['clear'](),console[_0x1cf7b1(0x1f9)](chalk[_0x1cf7b1(0x1ff)][_0x1cf7b1(0x1f7)](figlet[_0x1cf7b1(0x1f3)](_0x1cf7b1(0x200),{'font':_0x1cf7b1(0x201),'horizontalLayout':_0x1cf7b1(0x1f0),'verticalLayout':'default','width':0x50,'whitespaceBreak':![]}))),console['log'](chalk[_0x1cf7b1(0x204)](_0x1cf7b1(0x202)+chalk['yellow'](_0x1cf7b1(0x1f5))+'\x0a\x0a'+chalk[_0x1cf7b1(0x1e9)](_0x1cf7b1(0x200))+_0x1cf7b1(0x203)+chalk[_0x1cf7b1(0x1fa)](_0x1cf7b1(0x1fd))+'\x0a'+chalk[_0x1cf7b1(0x1e9)](_0x1cf7b1(0x1f1))+_0x1cf7b1(0x203)+chalk['white']('@deff.xyz')+'\x0a'+chalk['red'](_0x1cf7b1(0x1f4))+'\x20:\x20'+chalk[_0x1cf7b1(0x1fa)](_0x1cf7b1(0x1fb))+'\x0a'+chalk[_0x1cf7b1(0x1e9)](_0x1cf7b1(0x1ec))+_0x1cf7b1(0x203)+chalk[_0x1cf7b1(0x1fa)](_0x1cf7b1(0x1f6))+'\x0a'));}
 
 /**
 * Uncache if there is file change;
@@ -72,14 +64,14 @@ const status = new Spinner(chalk.cyan(` Booting WhatsApp Bot`))
 const starting = new Spinner(chalk.cyan(` Preparing After Connect`))
 const reconnect = new Spinner(chalk.redBright(` Reconnecting WhatsApp Bot`))
 
-const store = makeInMemoryStore({ logger: logg().child({ level: 'fatal', stream: 'store' }) })
+const store = makeInMemoryStore({ logger: logg().child({ level: 'warn', stream: 'store' }) })
 
 const connectToWhatsApp = async () => {
 	const conn = makeWASocket({
             printQRInTerminal: true,
-            logger: logg({ level: 'fatal' }),
+            logger: logg({ level: 'warn' }),
             auth: state,
-            browser: ["Hinata Hyuga Multi Device", "Safari", "3.0"]
+            browser: ["DEFF-MD", "Safari", "3.0"]
         })
 	title()
         store.bind(conn.ev)
@@ -100,9 +92,10 @@ const connectToWhatsApp = async () => {
 		var msg = m.messages[0]
 		msg = serialize(conn, msg)
 		msg.isBaileys = msg.key.id.startsWith('BAE5') || msg.key.id.startsWith('3EB0')
-		require('./message/msg')(conn, msg, m, setting, store, welcome)
+		require('./message/msg')(conn, msg, m, setting, store, )
 	})
-	conn.ev.on('group-participants.update', async (update) =>{
+
+conn.ev.on('group-participants.update', async (update) =>{
    groupResponse(conn, update)
    console.log(update)
    })         
